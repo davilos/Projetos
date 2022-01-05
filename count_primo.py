a5edc4ca1f7f52842aa3318a93f9dae13b16ce88
Dávios Tavares <daviloscostagg@hotmail.com>
2022-01-05 18:00:24 -0300

    Atualização

--- count_primo.py
+++ count_primo.py
@@ -7,7 +7,14 @@ def contador_de_primos(n):
 
 num_primos = 0
 
-for l in range(2, 12):
+# Opção 1 de input:
+# a, b = [int(input(f'Valor {_+1}: ')) for _ in range(2)]
+
+# Opção 2 de input:
+valores = input('Valores: ').split()
+x = list(map(int, valores))
+
+for l in range(x[0], x[1]):
     num_primos += contador_de_primos(l)
 
-print(num_primos)
+print(f'Os números primos existentes entre {x[0]} e {x[1]} é {num_primos}')
