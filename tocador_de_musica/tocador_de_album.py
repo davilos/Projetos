696698c05834a5e63c2237e142afa364f53849c7
davilos <daviloscostagg@hotmail.com>
2022-02-01 18:58:21 -0300

    Atualizado

--- tocador_de_musica/tocador_de_album.py
+++ tocador_de_musica/tocador_de_album.py
@@ -31,12 +31,22 @@ ano_do_album = list(
     )
 )
 
-# Realiza a pesquisa do álbum na lista "musicas".
-musicas_do_album = filter(lambda album: select_album.lower() in album.get("Nome").lower(), listas_tocador.musicas)
-
-# Desempacota somente as músicas da variável "musicas_do_album".
-musics = [[v for k, v in m.items() if k == 'Músicas'] for m in musicas_do_album]
-
+# Realiza a pesquisa do álbum na lista "musicas", pra depois tocar as músicas.
+"""
+musicas_do_album = list(
+    filter(
+        lambda album: select_album.lower() in album.get("Nome").lower()
+        , listas_tocador.musicas)
+)
+"""
+musicas_do_album = list(
+    map(lambda album: album["Músicas"],
+        filter(
+            lambda album: select_album.lower() in album.get("Nome").lower()
+            , listas_tocador.musicas)
+        )
+)
+print(musicas_do_album)
 print(f'\033[1;97m{select_album}\033[m')
 print('\033[1;97mArctic Monkeys\033[m')
 print(f'Álbum ° {ano_do_album[0]}')  # ano_do_album[0]["Ano"]
@@ -44,11 +54,11 @@ print(f'Álbum ° {ano_do_album[0]}')  # ano_do_album[0]["Ano"]
 options = int(input('[1] Ordenado [2] Aleatório: '))
 
 if options == 1:
-    for m in range(len(musics[0][0])):
-        print(musics[0][0][m])
+    for m in range(len(musicas_do_album[0])):
+        print(musicas_do_album[0][m])
         pygame.mixer.init()
         pygame.init()
-        pygame.mixer.music.load(f'{musics[0][0][m]}.mp3')
+        pygame.mixer.music.load(f'{musicas_do_album[0][m]}.mp3')
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             pass
@@ -56,8 +66,8 @@ if options == 1:
 else:
     random_list = []
     while len(random_list) < 13:
-        for m in range(len(musics[0][0])):
-            random_list.append(random.choice(musics[0][0]))
+        for m in range(len(musicas_do_album[0])):
+            random_list.append(random.choice(musicas_do_album[0][m]))
 
             for x in random_list:
                 while random_list.count(x) > 1:
