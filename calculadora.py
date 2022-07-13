36e59ee33e504416e0bdfb891b6f30122dd74cf4
davilos <daviloscostagg@hotmail.com>
2022-07-13 11:13:23 -0300

    Operador Walrus adicionado e correções de bugs

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-from typing import Union, Literal
+from typing import Union
 
 print('Calculadora')
 
@@ -22,60 +22,44 @@ try:
     op_1: str = input('Digite a operação: ').upper()
 
     if '.' in num_list[0] and '.' in num_list[1]:
-        print(aux := calculator(float(num_list[0]), float(num_list[2]), op_1))
+        print(aux := calculator(float(num_list[0]), float(num_list[1]), op_1))
 
-        while True:
-            op_2: str = input('Digite a operação: ').upper()
-            if op_2 != 'AC':
-                num_1: str = input('Digite o número: ')
-                if '.' in num_1:
-                    print(aux := calculator(aux, float(num_1), op_2, aux))
-                else:
-                    print(aux := calculator(aux, int(num_1), op_2, aux))
+        while (op_2 := input('Digite a operação: ').upper()) != 'AC':
+            num_1: str = input('Digite o número: ')
+            if '.' in num_1:
+                print(aux := calculator(aux, float(num_1), op_2, aux))
             else:
-                break
+                print(aux := calculator(aux, int(num_1), op_2, aux))
 
     elif '.' in num_list[0]:
         print(aux := calculator(float(num_list[0]), int(num_list[1]), op_1))
 
-        while True:
-            op_3: str = input('Digite a operação: ').upper()
-            if op_3 != 'AC':
-                num_2: str = input('Digite o número: ')
-                if '.' in num_2:
-                    print(aux := calculator(aux, float(num_2), op_3, aux))
-                else:
-                    print(aux := calculator(aux, int(num_2), op_3, aux))
+        while (op_3 := input('Digite a operação: ').upper()) != 'AC':
+            num_2: str = input('Digite o número: ')
+            if '.' in num_2:
+                print(aux := calculator(aux, float(num_2), op_3, aux))
             else:
-                break
+                print(aux := calculator(aux, int(num_2), op_3, aux))
 
     elif '.' in num_list[1]:
         print(aux := calculator(int(num_list[0]), float(num_list[1]), op_1))
 
-        while True:
-            op_4: str = input('Digite a operação: ').upper()
-            if op_4 != 'AC':
-                num_3: str = input('Digite o número: ')
-                if '.' in num_3:
-                    print(aux := calculator(aux, float(num_3), op_4, aux))
-                else:
-                    print(aux := calculator(aux, int(num_3), op_4, aux))
+        while (op_4 := input('Digite a operação: ').upper()) != 'AC':
+            num_3: str = input('Digite o número: ')
+            if '.' in num_3:
+                print(aux := calculator(aux, float(num_3), op_4, aux))
             else:
-                break
+                print(aux := calculator(aux, int(num_3), op_4, aux))
 
     else:
         print(aux := calculator(int(num_list[0]), int(num_list[1]), op_1))
 
-        while True:
-            op_5: str = input('Digite a operação: ').upper()
-            if op_5 != 'AC':
-                num_4: str = input('Digite o número: ')
-                if '.' in num_4:
-                    print(aux := calculator(aux, float(num_4), op_5, aux))
-                else:
-                    print(aux := calculator(aux, int(num_4), op_5, aux))
+        while (op_5 := input('Digite a operação: ').upper()) != 'AC':
+            num_4: str = input('Digite o número: ')
+            if '.' in num_4:
+                print(aux := calculator(aux, float(num_4), op_5, aux))
             else:
-                break
+                print(aux := calculator(aux, int(num_4), op_5, aux))
 
 except (TypeError, ValueError, IndexError) as erro:
     print(f'\033[31mHouve um erro: {erro}\033[m')
